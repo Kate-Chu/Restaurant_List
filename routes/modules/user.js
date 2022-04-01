709921ce80482573eef4f450ad28a503c4a147f3
KateChu <chu.shihchien@gmail.com>
2022-04-01 11:07:05 +0800

    feat: add user login page

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -5,4 +5,10 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.post("/login", (req, res) => {});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
 module.exports = router;
