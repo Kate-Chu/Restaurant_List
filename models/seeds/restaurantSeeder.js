e7a28c92f8fc898adc62cd317266f556a97b46b9
KateChu <chu.shihchien@gmail.com>
2022-04-01 10:32:37 +0800

    feat: add user modules

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,5 +1,6 @@
 const restaurantList = require("../restaurant.json");
 const db = require("../../config/mongoose");
+const Restaurants = require("../restaurant");
 
 db.once("open", () => {
   console.log("restaurantSeeder.js: MongoDB connected!");
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const home = require("./modules/home");
 const restaurants = require("./modules/restaurants");
+const users  = require("./modules/user")
 
-router.use("/", home);
 router.use("/restaurants", restaurants);
+router.use("/users", users);
+router.use("/", home);
 
 module.exports = router;
